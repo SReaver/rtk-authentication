adcaaa8901c49344c978dfdfe507a9cf83397511
Sergey Plotnikov <reaverz@yandex.ru>
2022-08-14 12:47:51 +0600

    initial commit

---
+++ src/app/api/apiSlice.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setCredentials, logOut } from '../../features/auth/authSlice'
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: 'http://localhost:3500',
+	credentials: 'include',
+	prepareHeaders: (headers, { getState }) => {
+		const token = getState().auth.token
+		if (token) {
+			headers.set('Authorization', `Bearer ${token}`)
+		}
+		return headers
+	}
+})
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+	let result = await baseQuery(args, api, extraOptions)
+	if (result?.error?.originalStatus === 403) {
+		//send refresh token to get new access token
+		const refreshResult = await baseQuery('/refresh', api, extraOptions)
+		console.log(refreshResult)
+		if (refreshResult?.data) {
+			const user = api.getState().auth.user
+			api.dispatch(setCredentials({ ...refreshResult.data, user }))
+			//retry original query with new token
+			result = await baseQuery(args, api, extraOptions)
+		}
+	} else {
+		api.dispatch(logOut())
+	}
+	return result
+}
+
+export const apiSlice = createApi({
+	baseQuery: baseQueryWithReauth,
+	endpoints: builder => ({})
+})
